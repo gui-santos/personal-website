6c42283d8b8d16af7db8a2ddab2399c51dce05a6
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2019-10-05 02:34:41 +0200

    add Layout component

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,15 +14,5 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        name: 'Gui Santos',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-      },
-    },
   ],
 }
