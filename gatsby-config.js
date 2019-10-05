231658c396c35d9a49ff22d6e8884b3304d6e74e
Guilherme Santos <guilherme.goncalvessantos@gmail.com>
2019-10-05 02:30:26 +0200

    add manifest again

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,15 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Gui Santos',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+      },
+    },
   ],
 }
